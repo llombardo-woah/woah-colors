ecb55543aaa1ff077283c4c46230a7e7eb464913
llombardo-woah <l.lombardo@woah.org>
2025-10-28 10:48:44 +0100

    Uploading R package for WOAH colors.

---
+++ R/woah_color_palette.R
@@ -0,0 +1,66 @@
+
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' WOAH Corporate Color Palette
+#'
+#' A named vector of HEX codes used by the World Organisation for Animal Health (WOAH)
+#' for consistent data visualization across reports, dashboards, and presentations.
+#'
+#' @format A named character vector with 50 HEX color codes.
+#'
+#' @examples
+#' woah_palette["Blue"]
+#'
+#' @export
+woah_palette <- c(
+  Orange     = "#FF4815",
+  Orange_10  = "#FF624A",
+  Orange_20  = "#FF7969",
+  Orange_30  = "#FF8C80",
+  Orange_40  = "#FF9E95",
+  Orange_50  = "#FFAFA9",
+  Orange_60  = "#FFC0BB",
+  Orange_70  = "#FFD0CD",
+  Orange_80  = "#FEE0DE",
+  Orange_90  = "#FFF0EF",
+  Black      = "#27282A",
+  Black_75   = "#565759",
+  Black_50   = "#8B8C8D",
+  Black_25   = "#C3C4C5",
+  Blue       = "#203CA9",
+  Blue_75    = "#5A69CA",
+  Blue_50    = "#8A97F7",
+  Blue_25    = "#C5CAFF",
+  Grey       = "#EFEEE8",
+  Grey_75    = "#F3F2EC",
+  Grey_50    = "#F8F7F1",
+  Grey_25    = "#FCFBF5",
+  Green      = "#009D5E",
+  Green_75   = "#42BA7B",
+  Green_50   = "#68D799",
+  Green_25   = "#8AF6B7",
+  LightBlue    = "#DCE9F4",
+  LightBlue_75 = "#E2EFFA",
+  LightBlue_50 = "#E9F5FF",
+  LightBlue_25 = "#F4FAFF",
+  LightGreen    = "#DDF3E6",
+  LightGreen_75 = "#E1F7EA",
+  LightGreen_50 = "#E6FCEF",
+  LightGreen_25 = "#ECFFF4",
+  Pink       = "#FFE7F0",
+  Pink_75    = "#FFEDF4",
+  Pink_50    = "#FFF3F7",
+  Pink_25    = "#FFF9FB",
+  Yellow     = "#FFF2CE",
+  Yellow_75  = "#FFF5DA",
+  Yellow_50  = "#FFF8E7",
+  Yellow_25  = "#FFFCF2"
+)
+
+
+
